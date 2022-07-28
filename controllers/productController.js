f223e4dc8a4ea01498dad355b086da0755032d4b
sourav326 <souravchauhan1964@gmail.com>
2022-07-28 16:12:37 +0530

    Add the productmodel and create product schema and create a POST Api to create the product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,16 @@
+const Product = require('../models/productModel');
+
 exports.getAllProducts =(req,res)=>{
-    res.status(200).json({message:"Route is working fine"});
+    res.status(200).json({
+        success:true,
+        message:"Route is working fine"
+    });
+}
+
+exports.createProduct = async(req,res,next)=>{
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success:true,
+        product
+    })
 }
---
+++ models/productModel.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true ,"Pleae enter product name"],
+        trim:true
+    },
+    description:{
+        type:String,
+        required:[true,"Please enter product description"]
+    },
+    price:{
+        type:Number,
+        required:[true,"Please enter product price"],
+        maxLength:[8,"Price cannot exceed 8 characters"]
+    },
+    rating:{
+        type:Number,
+        default:0
+    },
+    image:[
+        {
+            public_id:{
+                type:String,
+                required:true
+            },
+            url:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    category:{
+        type:String,
+        required:[true,"Please enter product category"]
+    },
+    stock:{
+        type:Number,
+        required:[true,"Please enter product stock"],
+        maxLength:[4,"Stock cannot exceed 4 characters"],
+        default:1
+    },
+    numOfReviews:{
+        type:Number,
+        default:0
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true
+            },
+            rating:{
+                type:Number,
+                required:true
+            },
+            comment:{
+                type:String,
+                required:true
+            }
+        }
+    ]
+    // createdAt:{
+    //     type:Date,
+    //     required:Date.now
+    // }
+})
+
+module.exports = mongoose.model("Product",productSchema);
